4319784553a9f92f9a2ae7938bd8c944d21f22dc
Akado2009 <prosvirov.k@gmail.com>
2019-03-28 10:49:01 -0700

    triangle finished

--- src/hello_window.cpp
+++ src/hello_window.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main() {
 
@@ -15,7 +18,7 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   // create window
-  GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hello World", NULL, NULL);
   if (window == NULL) {
     std::cout << "Failed to create GLFW Window" << std::endl;
     glfwTerminate();
@@ -31,6 +34,11 @@ int main() {
   }
 
   while(!glfwWindowShouldClose(window)){
+    processInput(window);
+
+    glClearColor(0.2f, 0.5f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
@@ -40,4 +48,10 @@ int main() {
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
   glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
 }
